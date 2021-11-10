40ad791324f3fa9402c0727ba9742689dc938b70
JubalThang <jubalthang@gmail.com>
2021-11-10 15:51:41 -0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([])
+  const [searchText, setSearchText] = useState('')
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+      .then(res => res.json())
+      .then(lists => setListings(lists))
+      .catch(err => console.error('Something wrong: ', err))
+  },[])
+
+  function handleDelete(id) {
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method : 'DELETE'
+    })
+
+    setListings(listings.filter(list => list.id !== id))
+
+  }
+
+  function handleSearch(text) {
+    setSearchText(text)
+  }
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header handleSearch={handleSearch} />
+      <ListingsContainer listings={listings} searchQuery={searchText} onDelete={handleDelete}/> 
+      {/* {listings.length !== 0 ? <ListingsContainer listings={listings} /> : <h1>Nothing found!</h1>} */}
     </div>
   );
 }
