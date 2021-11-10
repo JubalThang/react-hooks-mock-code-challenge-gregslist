2b8af08ff44a8722fd1a5564db917a00b4e4c4e7
JubalThang <jubalthang@gmail.com>
2021-11-10 15:51:08 -0800

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Search() {
+function Search({searchQuery}) {
+ 
+
+  const [searchText, setSearchText] = useState('')
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("submitted");
+    searchQuery(searchText)
+  }
+
+  function handleSearchText(e) {
+    setSearchText(e.target.value)
   }
 
   return (
@@ -12,8 +20,8 @@ function Search() {
         type="text"
         id="search"
         placeholder="search free stuff"
-        value={""}
-        onChange={(e) => console.log(e.target.value)}
+        value={searchText}
+        onChange={handleSearchText}
       />
       <button type="submit">🔍</button>
     </form>
