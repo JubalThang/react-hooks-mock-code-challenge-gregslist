96b69c717bb738bc42a49f5037f752a6064386ef
JubalThang <jubalthang@gmail.com>
2021-11-10 15:50:50 -0800

    Update ListingsContainer.js

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,26 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+
+import ListingCard from "./ListingCard";
+
+function ListingsContainer({listings, searchQuery, onDelete}) {
+
+
+  const filtered =  listings.filter(list => 
+        list.description.toLowerCase().includes(searchQuery.toLowerCase()))
+    
+  
+  
   return (
+   
     <main>
       <ul className="cards">
         {/* use the ListingCard component to display listings */}
+       {filtered.length !== 0 ?
+         filtered.map(list => <ListingCard key={list.id} list={list} onDelete={onDelete}/>)
+         :
+         <h1>No listiongs found!</h1>
+       }
+       
       </ul>
     </main>
   );
